8d33c318b5575589f9c65cbd295ec86ae5e632d6
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-07-03 20:47:35 +0900

    Update, const anything_month add.

--- denops/himekuri/main.ts
+++ denops/himekuri/main.ts
@@ -8,13 +8,13 @@ export async function main(denops: Denops): Promise<void> {
             let comma = ' : '
             let str = '時刻を表示'
             let OneYear = OneDay.getFullYear()
-            const OneMonth = OneDay.getMonth() + 1
+            const anything_month = OneDay.getMonth()
+            const OneMonth = (anything_month + 1)
             let anyDays = OneDay.getDate()
             let OneHour = OneDay.getHours()
             let OneMinute = OneDay.getMinutes()
             let OneSecond = OneDay.getSeconds()
             let WeekDay = OneDay.getDay()
-            let anything_month = OneDay.getMonth()
             let anyMonth = ['睦月', '如月', '弥生', '卯月', '皐月', '水無月', '文月', '葉月', '長月', '神無月', '霜月', '師走']
             let something_Month = anyMonth[anything_month]
 
