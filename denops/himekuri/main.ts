dbf7b11882a607ecfec1a9d935d4c9b80407b00a
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-07-07 07:53:44 +0900

    Update, import version down.

--- denops/himekuri/main.ts
+++ denops/himekuri/main.ts
@@ -1,5 +1,5 @@
 import {Denops} from 'https://deno.land/x/denops_std@v1.0.0-beta.1/mod.ts';
-import {execute} from 'https://deno.land/x/denops_std@v1.0.0-beta.1/helper/mod.ts';
+import {execute} from 'https://deno.land/x/denops_std@v1.0.0-beta.0/helper/mod.ts';
 
 export async function main(denops: Denops): Promise<void> {
     denops.dispatcher = {
